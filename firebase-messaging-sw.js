dd96caac06ea89ae53ba8aa05edcda58bce5f593
Pato Vargas <pevargas2730@gmail.com>
2020-01-09 01:47:48 -0600

    pwa notification working

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  messagingSenderId: '937961744284'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  // Customize notification here
+  const notification = JSON.parse(payload.data.notification);
+  const notificationTitle = notification.title;
+  const notificationOptions = {
+    body: notification.body
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
