27940f62709492ef4a477eb65a5360f675858df8
Pato <pevargas2730@gmail.com>
2020-01-21 03:08:32 -0500

    nofitifaction working

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js");
 // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js');
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js");
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: '937961744284'
+  messagingSenderId: "937961744284"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
+messaging.setBackgroundMessageHandler(payload => {
   console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
+    "[firebase-messaging-sw.js] Received background message ",
+    payload.notification.body
   );
   // Customize notification here
   const notification = JSON.parse(payload.data.notification);
